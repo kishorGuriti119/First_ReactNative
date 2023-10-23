d040df647559d6505fe1ec7a27606750a161b761
kishor119 <kishior.guriti@motivitylabs.com>
2023-10-23 16:27:01 +0530

    implementing appheader

--- src/Screens/InterfaceScreens/Home/index.jsx
+++ src/Screens/InterfaceScreens/Home/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {style} from './style';
+import AppHeader from '../../../Components/AppHeader';
 
 const Home = () => {
+  const [openSearchInput, setSearchInput] = useState(false);
+
+  const toogleSearchInput = () => {
+    setSearchInput(!openSearchInput);
+  };
+
   return (
     <SafeAreaView>
       <ScrollView style={style.Container}>
-        <Text>Home</Text>
+        <AppHeader
+          search
+          title="Find All You Need"
+          onSearchClick={toogleSearchInput}
+        />
       </ScrollView>
     </SafeAreaView>
   );
